73aede488f3fb1789053e6acb761c9416c999336
JoanUniverse <jonnyy184@gmail.com>
2020-03-11 12:56:51 +0100

    first commit

---
+++ W20_QuetglasWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace W20_QuetglasWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ W20_QuetglasWebAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(W20_QuetglasWebAPI.Startup))]
+
+namespace W20_QuetglasWebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
